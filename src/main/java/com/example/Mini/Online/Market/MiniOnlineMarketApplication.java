8c11b6d9ab93e880f250226f2fcf4e0d82fcb0d3
Rebecca <rebeccayilma@gmail.com>
2021-09-20 17:48:02 -0500

    Initial commit

---
+++ src/main/java/com/example/Mini/Online/Market/MiniOnlineMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mini.Online.Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOnlineMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniOnlineMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mini/Online/Market/MiniOnlineMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mini.Online.Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOnlineMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
