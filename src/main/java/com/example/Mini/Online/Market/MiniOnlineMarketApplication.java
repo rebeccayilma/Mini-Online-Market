0bcb75344c50ffef811f147b46a2c93dd09d0dbb
Rebecca <rebeccayilma@gmail.com>
2021-09-23 14:23:29 -0500

    cors

--- src/main/java/com/example/Mini/Online/Market/MiniOnlineMarketApplication.java
+++ src/main/java/com/example/Mini/Online/Market/MiniOnlineMarketApplication.java
@@ -2,12 +2,40 @@ package com.example.Mini.Online.Market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MiniOnlineMarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniOnlineMarketApplication.class, args);
+	}
+
+//	@Bean
+//	CorsConfigurationSource corsConfigurationSource() {
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		config.addAllowedOrigin("*");
+//		config.setAllowCredentials(true);
+//		source.registerCorsConfiguration("/**", config);
+//		return source;
+//	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("*").allowedOrigins("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
