8c011d15c8347f5da39d9bd2f7b512f72c51951d
Rebecca <rebeccayilma@gmail.com>
2021-09-23 22:18:59 -0500

    update

--- src/main/java/com/example/Mini/Online/Market/domain/Address.java
+++ src/main/java/com/example/Mini/Online/Market/domain/Address.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -22,6 +19,8 @@ public class Address {
     private String street;
     private String city;
     private String state;
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private User user;
 
     @Override
     public String toString() {
